86a77ce4bb2002b31666da676a7e2c1ad34dd2ee
wikipedia <ramdhaniabdulaziz4321@gmail.com>
2025-07-16 12:15:35 +0700

    Add files via upload

---
+++ Dhani.js
@@ -0,0 +1,10 @@
+// Saat nama Windah Basudara diklik
+document.getElementById("klikNama").addEventListener("click", function() {
+  alert("Windah Basudara adalah seorang YouTuber gaming terkenal di Indonesia.");
+  console.log("Nama Windah diklik.");
+});
+
+// Saat halaman dimuat
+window.addEventListener("load", function() {
+  console.log("Halaman Windah Basudara telah dimuat.");
+});
